a62c2dd513716f6ec6c7d842596ac09c2b5a4516
blubrandi <blubrandi0@gmail.com>
2019-03-07 22:46:06 -0700

    completed day 1 mvp, except for clear button.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import React from 'react';
 
+import TodoList from './components/TodoComponents/TodoList'
+import TodoForm from './components/TodoComponents/TodoForm'
+// import Todo from './components/TodoComponents/Todo'
+
+const todosArray = []
+
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  
+  constructor() {
+    super()
+    this.state = {
+      todos: todosArray,
+      todo: '',
+    }
+    
+  }
+
+  handleChanges = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  addNewTask = event => {
+    event.preventDefault();
+    const newTask = { task: this.state.todo, completed: false, id: Date.now()}
+    this.setState({
+      todos: [...this.state.todos, newTask],
+      todo: '',
+    })
+  }
+
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>Testing Output</h1>
+        <TodoList todos = {this.state.todos} />
+        <TodoForm 
+          value={this.state.todo}
+          addNewTask={this.addNewTask}
+          handleChanges={this.handleChanges}/>
       </div>
     );
   }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function TodoForm(props) {
+    return(
+        <form onSubmit={props.addNewTask}>
+        <input
+            value={props.value}
+            type='text'
+            name='todo'
+            placeholder='Add New Item'
+            onChange={props.handleChanges}
+        />
+        <button onClick = {props.addNewTask} type="submit">Add New Task</button>
+        </form>
+    )
+}
+
+export default TodoForm
